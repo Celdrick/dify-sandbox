14fbd74a7e799a4edc1372b6eac21ff11cc513f4
Celdrick <594596998@qq.com>
2026-03-13 09:24:17 +0800

    Add symlinks for npm and npx in entrypoint script

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,6 +4,8 @@
 tar -xvf $NODE_TAR_XZ -C /opt
 mkdir -p /usr/local/bin
 ln -s $NODE_DIR/bin/node /usr/local/bin/node
+ln -s $NODE_DIR/bin/npm /usr/local/bin/npm
+ln -s $NODE_DIR/bin/npx /usr/local/bin/npx
 rm -f $NODE_TAR_XZ
 
 # start main
